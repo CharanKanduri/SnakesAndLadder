6228bae6c52467dc3cdd34aa911760af11e039e1
charan_kanduri <charan2000kanduri@gmail.com>
2021-06-30 20:14:48 +0530

    [CHARAN]UC-4 Upto 100

--- SnakesAndLadder/Program.cs
+++ SnakesAndLadder/Program.cs
@@ -1,39 +1,47 @@
 ﻿using System;
 
+
 namespace SnakesAndLadder
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             int playerPosition = 0;
-            Random random = new Random();
-            int dieValue=random.Next(1,7);
-            Console.WriteLine("Number Obtained is:"+ dieValue);
 
-            int optionCheck = random.Next(0,3);
-
-        
-
-            if (optionCheck == 0) 
+            while (playerPosition <= 100)
             {
-                Console.WriteLine("Player Option is No play");
-                Console.WriteLine("Player Current position is: "+playerPosition);
+                Random random = new Random();
+                int dieValue = random.Next(1, 7);
+                Console.WriteLine("Number Obtained is:" + dieValue);
+
+                int optionCheck = random.Next(0, 3);
+
+                if (optionCheck == 0)
+                {
+                    Console.WriteLine("Player Option is No play");
+                    Console.WriteLine("Player Current position is: " + playerPosition);
+                    if (playerPosition < 0) playerPosition = 0;
+                }
+
+                if (optionCheck == 1)
+                {
+                    Console.WriteLine("Player Option is Ladder");
+                    playerPosition = playerPosition + dieValue;
+                    Console.WriteLine("Player Current position is: " + playerPosition);
+                }
+
+                if (optionCheck == 2)
+                {
+                    Console.WriteLine("Player Option is Snake :-(");
+                    playerPosition = playerPosition - dieValue;
+                    if (playerPosition < 0) playerPosition = 0;
+                    Console.WriteLine("Player Current position is: " + playerPosition);
+
+                }
             }
 
-            if (optionCheck == 1)
-            {
-                Console.WriteLine("Player Option is Ladder");
-                playerPosition = playerPosition + dieValue;
-                Console.WriteLine("Player Current position is: " + playerPosition);
-            }
-
-            if (optionCheck == 2)
-            {
-                Console.WriteLine("Player Option is Snake :-(");
-                playerPosition = playerPosition - dieValue;
-                Console.WriteLine("Player Current position is: " + playerPosition);
-            }
         }
     }
 }
