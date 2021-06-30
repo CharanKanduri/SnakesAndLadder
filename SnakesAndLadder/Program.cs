7a6d2216d1da4efae4370475815567447d499f06
charan_kanduri <charan2000kanduri@gmail.com>
2021-06-30 19:43:42 +0530

    [CHARAN]UC-2 Rolling a Dice

--- SnakesAndLadder/Program.cs
+++ SnakesAndLadder/Program.cs
@@ -7,6 +7,9 @@ namespace SnakesAndLadder
         static void Main(string[] args)
         {
             int playerPosition = 0;
+            Random random = new Random();
+            int dieValue=random.Next(1,7);
+            Console.WriteLine("Number Obtained is:"+ dieValue);
         }
     }
 }
