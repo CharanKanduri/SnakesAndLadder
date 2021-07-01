c501bb4fe3defc423573206a48366d2763f24b1b
charan_kanduri <charan2000kanduri@gmail.com>
2021-07-01 19:51:33 +0530

    [CHARAN] Added Comments in UC-2

--- SnakesAndLadder/Program.cs
+++ SnakesAndLadder/Program.cs
@@ -7,6 +7,8 @@ namespace SnakesAndLadder
         static void Main(string[] args)
         {
             int playerPosition = 0;
+
+            //UC-2 using random class to pick a number from 1 -6
             Random random = new Random();
             int dieValue=random.Next(1,7);
             Console.WriteLine("Number Obtained is:"+ dieValue);
